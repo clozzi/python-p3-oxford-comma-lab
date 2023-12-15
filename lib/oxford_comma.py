5bb21adc971877a5e53b2231fed69939f649125c
Chris Lozzi <clozzi12@gmail.com>
2023-12-15 12:58:23 -0500

    missing big test

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return "".join(items)
+    if len(items) == 2:
+        return items[0] + " and " + items[1]
+    if len(items) == 3:
+        return items[0] + ", " + items[1] + ", and " + items[2]
+    if len(items) > 3:
+
+
+
+# if index -2 add and instead of ,
